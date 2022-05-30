063d10bd8a8aa871c84374a2b310ffccc203e0c9
bun579 <106530979+bun579@users.noreply.github.com>
2022-05-30 17:26:20 +0530

    Add files via upload

---
+++ passwordtohash.py
@@ -0,0 +1,17 @@
+import hashlib, bcrypt
+password = input("Input the password to hash\n>")
+print("\nSHA1:\n")
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.sha1(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+print("\nMD5:\n")
+for i in range(3):
+    setpass = bytes(password, 'utf-8')
+    hash_object = hashlib.md5(setpass)
+    guess_pw = hash_object.hexdigest()
+    print(guess_pw)
+print("\nBCRYPT:\n")
+for i in range(3):
+    hashed = bcrypt.hashpw(setpass, bcrypt.gensalt(10))
